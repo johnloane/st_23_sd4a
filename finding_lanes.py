21837feb5cc881c6a9724056de28075cf0a178f3
John Loane <john.loane@dkit.ie>
2023-10-02 15:39:44 +0100

    Finding lane lines
    Edit parameters to make lane lines fit better and remove noise

--- finding_lanes.py
+++ finding_lanes.py
@@ -6,8 +6,53 @@ def main():
     image = cv2.imread('road.jpg')
     lane_image = np.copy(image)
     canny_image = find_edges(lane_image)
-    cv2.imshow("ROI", region_of_interest(canny_image))
+    cropped_image = region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 40, np.array([]), 
+                            minLineLength=5, maxLineGap = 1)
+    averaged_lines = average_slope_intercept(lane_image, lines)
+    line_image = display_lines(lane_image, averaged_lines)
+    combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+    cv2.imshow("Combo image", combo_image)
     cv2.waitKey(0)
+
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis = 0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+
+
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int((y1 * 3/5))
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return np.array([x1, y1, x2, y2])
+
+
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (0, 128, 0), 2) #bgr
+    return line_image
+
 
 
 def region_of_interest(image):
@@ -17,7 +62,8 @@ def region_of_interest(image):
     ])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, triangle, 255)
-    return mask
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
 
 def find_edges(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
