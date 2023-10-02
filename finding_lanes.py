48e9d2d36355e0d082a3d69404ad96e7cae29c65
John Loane <john.loane@dkit.ie>
2023-10-02 16:58:18 +0100

    Finding lane lines in a video
    Homework: Play with the parameters to get the lane lines for the
    image better. Need to try and remove some of the noise from the
    current version.

--- finding_lanes.py
+++ finding_lanes.py
@@ -3,23 +3,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    image = cv2.imread('road.jpg')
-    lane_image = np.copy(image)
-    canny_image = find_edges(lane_image)
-    cropped_image = region_of_interest(canny_image)
-    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 40, np.array([]), 
-                            minLineLength=5, maxLineGap = 1)
-    averaged_lines = average_slope_intercept(lane_image, lines)
-    line_image = display_lines(lane_image, averaged_lines)
-    combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-    cv2.imshow("Combo image", combo_image)
-    cv2.waitKey(0)
+    #image = cv2.imread('road.jpg')
+    cap = cv2.VideoCapture('JohnM50.mp4')
+    while(cap.isOpened()):
+        _, frame = cap.read()
+        if frame is not None:
+            print(frame.shape)
+            frame = cv2.resize(frame, (960, 1280))
+            lane_image = np.copy(frame)
+            canny_image = find_edges(lane_image)
+            cropped_image = region_of_interest(canny_image)
+            lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), 
+                                minLineLength=40, maxLineGap = 100)
+            averaged_lines = average_slope_intercept(lane_image, lines)
+            line_image = display_lines(lane_image, averaged_lines)
+            combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+            cv2.imshow("Combo image", combo_image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 def average_slope_intercept(image, lines):
     left_fit = []
     right_fit = []
-
+    if lines is None:
+        return None
     for line in lines:
         x1, y1, x2, y2 = line.reshape(4)
         parameters = np.polyfit((x1, x2), (y1, y2), 1)
@@ -37,12 +49,16 @@ def average_slope_intercept(image, lines):
 
 
 def make_coordinates(image, line_parameters):
+    print(line_parameters)
+    if line_parameters is None:
+        return None
     slope, intercept = line_parameters
-    y1 = image.shape[0]
-    y2 = int((y1 * 3/5))
-    x1 = int((y1 - intercept)/slope)
-    x2 = int((y2 - intercept)/slope)
-    return np.array([x1, y1, x2, y2])
+    if line_parameters is not None and abs(slope) > 0.001:
+        y1 = image.shape[1]
+        y2 = int((y1 * 9/20))
+        x1 = int((y1 - intercept)/slope)
+        x2 = int((y2 - intercept)/slope)
+        return np.array([x1, y1, x2, y2])
 
 
 def display_lines(image, lines):
@@ -58,7 +74,7 @@ def display_lines(image, lines):
 def region_of_interest(image):
     height = image.shape[0]
     triangle = np.array([
-        [(0, height), (1000, height), (650, 200)]
+        [(0, 700), (500, 300), (960, 1000)]
     ])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, triangle, 255)
@@ -68,7 +84,7 @@ def region_of_interest(image):
 def find_edges(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    canny_image = cv2.Canny(blur, 50, 100)
+    canny_image = cv2.Canny(blur, 50, 150)
     return canny_image
     
 
